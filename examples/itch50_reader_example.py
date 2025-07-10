8ff9cea0d49cb6ae6c36c7830252a2b518745975
Vincent Grégoire <vincent.gregoire@gmail.com>
2025-07-10 14:04:48 -0400

    Added JSON support to ITCH50 parser

--- examples/itch50_reader_example.py
+++ examples/itch50_reader_example.py
@@ -3,10 +3,13 @@ from pathlib import Path
 from meatpy.itch50 import ITCH50Parser
 
 file_path = Path("/Users/vincentgregoire/Documents/Data/itch/20190530.BX_ITCH_50.gz")
-file_path = Path("/Users/vincentgregoire/Documents/Data/itch/S081321-v50.txt.gz")
+# file_path = Path("/Users/vincentgregoire/Documents/Data/itch/S081321-v50.txt.gz")
 
 with ITCH50Parser(file_path) as parser:
     for i, message in enumerate(parser):
-        if i > 1000:
-            break
-        print(message)
+        # if i > 1000:
+        #     break
+        # print(message.to_json())
+        x = message.to_json()
+        if i % 1_000_000 == 0:
+            print(f"Processed {i:,d} messages")
